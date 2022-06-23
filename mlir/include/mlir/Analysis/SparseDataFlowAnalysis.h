ef55eb3d7410b86eac8e796adcb1dc1b258baa45
Mogball <jeffniu22@gmail.com>
2022-06-23 19:03:12 +0000

    [mlir] An implementation of dense data-flow analysis
    This patch introduces an implementation of dense data-flow analysis. Dense
    data-flow analysis attaches a lattice before and after the execution of every
    operation. The lattice state is propagated across operations by a user-defined
    transfer function. The state is joined across control-flow and callgraph edges.
    
    Thge patch provides an example pass that uses both a dense and a sparse analysis
    together.

--- mlir/include/mlir/Analysis/SparseDataFlowAnalysis.h
+++ mlir/include/mlir/Analysis/SparseDataFlowAnalysis.h
@@ -81,11 +81,10 @@ private:
 template <typename ValueT>
 class Lattice : public AbstractSparseLattice {
 public:
-  using AbstractSparseLattice::AbstractSparseLattice;
-
-  /// Get a lattice element with a known value.
-  Lattice(const ValueT &knownValue = ValueT())
-      : AbstractSparseLattice(Value()), knownValue(knownValue) {}
+  /// Construct a lattice with a known value.
+  explicit Lattice(Value value)
+      : AbstractSparseLattice(value),
+        knownValue(ValueT::getPessimisticValueState(value)) {}
 
   /// Return the value held by this lattice. This requires that the value is
   /// initialized.
@@ -235,6 +234,9 @@ private:
 /// This lattice value represents a known constant value of a lattice.
 class ConstantValue {
 public:
+  /// The pessimistic value state of the constant value is unknown.
+  static ConstantValue getPessimisticValueState(Value value) { return {}; }
+
   /// Construct a constant value with a known constant.
   ConstantValue(Attribute knownValue = {}, Dialect *dialect = nullptr)
       : constant(knownValue), dialect(dialect) {}
